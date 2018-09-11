da8cccedd4969bbbd28a227c2ec40626d69ce1ae
EastSummer <369701992@qq.com>
2018-09-11 14:15:30 +0800

    Add home-redux

---
+++ jianshu/src/pages/home/store/constants.js
@@ -0,0 +1,1 @@
+export const SEARCH_FOCUS = 'home/SEARCH_FOCUS'
--- jianshu/src/store/reducer.js
+++ jianshu/src/store/reducer.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux-immutable';
 import { reducer as headerReducer } from '../common/header/store';
+import { reducer as homeReducer } from '../pages/home/store';
 
 // 合并各个小的reducer
 const reducer = combineReducers({
-    header: headerReducer
+    header: headerReducer,
+    home: homeReducer
 })
 
 export default reducer
